63c56829b35eb455df3cf9762ab76f740f8961f2
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2016-07-26 01:21:34 -0300

    Property and Event binding on AppComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: 
+    `
+    <h1>{{title}}</h1>
+    <div [style.color]="nameColor">
+        Hello {{name}}
+    </div>
+    <button (click)="changeNameColor()"> Change name color </button>
+    `
 })
-export class AppComponent { }
+export class AppComponent { 
+    // [ ] PROPERTY BINDING - one way binding from the component to the dom
+    // ( ) EVENT BINDING - from the dom to the component
+    // NOTE: both auto refers to the component scope
+
+    title = "Customer App";
+    name = "Leonardo";
+    nameColor = "red";
+
+    changeNameColor() {
+        this.nameColor = (this.nameColor === "red" ? "blue" : "red");
+    }
+}

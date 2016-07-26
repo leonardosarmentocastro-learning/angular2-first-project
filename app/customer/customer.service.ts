afa8c79682c8c27e1b0110353449f029f0444807
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2016-07-26 05:45:11 -0300

    Presenting 4 ways of fetching data with http
    - Observables (modifying the array)
    - Observables + template async pipe
    - Promises (modifying the array)
    - Promises + template async pipe

--- app/customer/customer.service.ts
+++ app/customer/customer.service.ts
@@ -16,12 +16,23 @@ export class CustomerService {
     constructor(private _http: Http) { }
 
     getCustomers() {
-        // With observables you can get a stream of just a tinny bit of information you want
-        // As you don't want to the response to get the code, body or whatever so, you just want the json
-        // There it is
         return this._http.get(URL_CUSTOMER)
             .map((response: Response) => response.json() )
-            .catch(this._handleError);
+            .toPromise()
+            .catch((err: any) => {
+                console.log(err); // Please don't do that, customize me pls
+                return Promise.reject(err);
+            }
+        );
+    }
+
+    getCustomers_RxObservables() {
+    // With observables you can get a stream of just a tinny bit of information you want
+    // As you don't want to the response to get the code, body or whatever so, you just want the json
+    // There it is
+    return this._http.get(URL_CUSTOMER)
+        .map((response: Response) => response.json() )
+        .catch(this._handleError);
     }
 
     _handleError(err: any) {
--- app/customer/customers.component.ts
+++ app/customer/customers.component.ts
@@ -16,7 +16,15 @@ import { CustomerService } from './customer.service';
     ]
 })
 export class CustomersComponent implements OnInit {
-    customers: Observable<any[]>;
+    // EXAMPLE 1:
+    // customers: Observable<any[]>;
+    
+    // EXAMPLE 2:
+    // customers: Promise<any[]>;
+    
+    // EXAMPLE 3:
+    customers: any[];
+
 
     // ==> NOTE - INSTEAD OF THIS:
     // private _customerService: CustomerService;
@@ -24,19 +32,59 @@ export class CustomersComponent implements OnInit {
     // constructor(customerService: CustomerService) { 
     //     this._customerService = customerService;
     // }
-
     // ==> DO THIS:
     constructor(private _customerService: CustomerService) {}
 
     ngOnInit() { 
         // do the work here, not on the constructor
-        this.customers = this._customerService.getCustomers()
-        .catch((err: any) => {
-            console.log(err); // Don't do this, show the user a nice message
+        
+        // EXAMPLE 4:
+        // customers: any[];
+        // ==> NOTE: Don't use the "async pipe" on your template here as you don't have a Observable of any[] type "customers" 
+        this._customerService.getCustomers_RxObservables()
+        .subscribe(
+            // success function    
+            (customers) => {
+                // Here you can manipulate your "customers: any[]" array the way whatever you want to
+                this.customers = customers;
+            },
+            // error function
+            (err: any) => {
+                console.log(err); // Don't do this, show the user a nice message
+            }
+        );
+
+
+        // EXAMPLE 3:
+        // customers: any[];
+        // ==> NOTE: Don't use the "async pipe" on your template here as you don't have a Promise of any[] type "customers" 
+        // this._customerService.getCustomers()
+        // .then((customers) => {
+        //     // Here you can manipulate your "customers: any[]" array the way whatever you want to
+        //     this.customers = customers;
+        // })
+        // .catch((err: any) => {
+        //     console.log(err); // Don't do this, show the user a nice message
+        // });
+
+        // EXAMPLE 2: 
+        // customers: Promise<any[]>; way 
+        // => NOTE: (use the "async pipe" on template and do this if you don't need to manipulate the returned json object)
+        // this.customers = this._customerService.getCustomers()
+        // .catch((err: any) => {
+        //     console.log(err); // Don't do this, show the user a nice message
+        // });
+
+        // EXAMPLE 1:
+        // customers: Observable<any[]>; way 
+        // => NOTE: (use the "async pipe" on template and do this if you don't need to manipulate the returned json object)
+        // this.customers = this._customerService.getCustomers()
+        // .catch((err: any) => {
+        //     console.log(err); // Don't do this, show the user a nice message
             
-            // Now eat it but only if the message was communicated to the user
-            return Observable.of([]);
-        });
+        //     // Now eat it but only if the message was communicated to the user
+        //     return Observable.of([]);
+        // });
     }
 
 }

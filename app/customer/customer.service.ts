dd4b4da394e108dc5c70615a71f5340ee9467bab
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2016-07-26 05:17:37 -0300

    Error handling with observables

--- app/customer/customer.service.ts
+++ app/customer/customer.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
+import { Observable } from 'rxjs/Rx';
+
 // Add everything about RxJs
 // import "rxjs/Rx";
 // Add just a few operators of RxJs
@@ -18,7 +20,13 @@ export class CustomerService {
         // As you don't want to the response to get the code, body or whatever so, you just want the json
         // There it is
         return this._http.get(URL_CUSTOMER)
-            .map((response: Response) => response.json() );
+            .map((response: Response) => response.json() )
+            .catch(this._handleError);
+    }
+
+    _handleError(err: any) {
+        console.log(err); // log it somewhere else and format the message for devs
+        return Observable.throw(err); // our opportunity to customize this error
     }
 
 }
--- app/customer/customers.component.ts
+++ app/customer/customers.component.ts
@@ -30,7 +30,13 @@ export class CustomersComponent implements OnInit {
 
     ngOnInit() { 
         // do the work here, not on the constructor
-        this.customers = this._customerService.getCustomers();
+        this.customers = this._customerService.getCustomers()
+        .catch((err: any) => {
+            console.log(err); // Don't do this, show the user a nice message
+            
+            // Now eat it but only if the message was communicated to the user
+            return Observable.of([]);
+        });
     }
 
 }

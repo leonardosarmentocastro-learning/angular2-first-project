7e6c18ef2c3bbacaea12a5460da095a08b192b02
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2016-07-26 04:08:04 -0300

    Using module.id to not dealing with template path issues

---
+++ app/app.d.ts
@@ -0,0 +1,4 @@
+// => Declare here your custom global variables
+
+// System.js variable that help us not needing to type the entire path for components
+// declare var __moduleName: any;
--- app/customer/customer.component.ts
+++ app/customer/customer.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 
 // Note that the "export" statement is very important so you can use it externaly on others components
 @Component({
+    moduleId: module.id,
     selector: 'app-customer',
-    templateUrl: 'app/customer/customer.component.html'
+    templateUrl: 'customer.component.html'
 })
 export class CustomerComponent implements OnInit {
     // This @Input decorator tells that the "app-customer" component
